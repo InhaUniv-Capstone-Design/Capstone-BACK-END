24c722594e21f97a738df928cb4441dbff5729b7
허윤석 <hin0106@naver.com>
2026-09-25 21:54:04 +0900

    chore: Spring Boot 초기 프로젝트 설정

---
+++ src/main/java/com/gamcho/yakyeon/YakyeonApplication.java
@@ -0,0 +1,13 @@
+package com.gamcho.yakyeon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YakyeonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YakyeonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gamcho/yakyeon/YakyeonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamcho.yakyeon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YakyeonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
